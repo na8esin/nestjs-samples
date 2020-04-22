f697f0ffeb4be4e572637f746d73c0ddbdd1c641
na8esin <na8esin.mpst@gmail.com>
2020-04-22 15:13:46 +0900

    moduleをimportした簡単なテスト

--- 12-graphql-schema-first/src/cats/cats.service.spec.ts
+++ 12-graphql-schema-first/src/cats/cats.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test } from '@nestjs/testing';
 import { CatsModule } from './cats.module';
 import { CatsService } from './cats.service';
+import { Cat } from 'src/graphql.schema';
 
 describe('AgentsService', () => {
   let catsService: CatsService;
@@ -9,18 +10,13 @@ describe('AgentsService', () => {
     const moduleRef = await Test.createTestingModule({
       imports: [CatsModule]
     }).compile();
-    agentsService = moduleRef.get<AgentsService>(AgentsService);
+    catsService = moduleRef.get<CatsService>(CatsService);
   });
 
   describe('findAll', () => {
     it('should return an array of agents', async () => {
-      const agents: Agent[] = agentsService.findAll();
-      console.log(agents);
-
-      // アローメソッドでも問題なく動く
-      console.log(await agentsService.batchLoadFn(2));
-
-      expect(agents).toEqual(agentsDummy);
+      const cats: Cat[] = catsService.findAll();
+      console.log(cats);
     });
   });
 });
